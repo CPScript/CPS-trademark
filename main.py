32485e0220d924150ee55ea25e3c2ee0d4449124
Chai <83523587+CPScript@users.noreply.github.com>
2022-12-05 11:22:09 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import pyfiglet
+from termcolor import colored
+
+
+text = colored("  ▄▀ ▄▀", "green")
+print(text)
+text = colored("   ▀  ▀", "green")
+print(text)
+text = colored(" █▀▀▀▀▀█▄", "green")
+print(text)
+text = colored(" █░░░░░█ █", "green")
+print(text)
+text = colored(" ▀▄▄▄▄▄▀▀", "green")
+print(text)
+text = colored(">CPScript<", "blue")
+print(text)
+print(" ")
+print(" ")
+print(" ")
